91ee688cc1b4b5ed5a8a21ef63a3792f748d806a
AFuture <afuture.d@outlook.com>
2024-09-12 03:28:27 +0800

    feat

--- scripts/build-mmkv.sh
+++ scripts/build-mmkv.sh
@@ -10,9 +10,9 @@ echo "BUILD_TAG: $BUILD_TAG"
 echo "MMKV_REPO_DIR: $MMKV_REPO_DIR"
 echo "MMKV_XC_MANIFEST_NAME: $MMKV_XC_MANIFEST_NAME"
 
-echo "---"
+echo "---------------------------------"
 echo  "ls $(ls -la)"
-echo "---"
+echo "---------------------------------"
 
 pushd "$MMKV_REPO_DIR" > /dev/null
 echo "[*] Changed to $MMKV_REPO_DIR $PWD"
@@ -27,39 +27,39 @@ xcodebuild archive \
     SKIP_INSTALL=NO \
     BUILD_LIBRARY_FOR_DISTRIBUTION=YES
 
-xcodebuild archive \
-    -workspace MMKV.xcworkspace \
-    -scheme MMKV \
-    -configuration Release \
-    -destination "generic/platform=iOS Simulator" \
-    -archivePath "archives/MMKV-iOS-Simulator" \
-    SKIP_INSTALL=NO \
-    BUILD_LIBRARY_FOR_DISTRIBUTION=YES
+# xcodebuild archive \
+#     -workspace MMKV.xcworkspace \
+#     -scheme MMKV \
+#     -configuration Release \
+#     -destination "generic/platform=iOS Simulator" \
+#     -archivePath "archives/MMKV-iOS-Simulator" \
+#     SKIP_INSTALL=NO \
+#     BUILD_LIBRARY_FOR_DISTRIBUTION=YES
 
-xcodebuild archive \
-    -workspace MMKV.xcworkspace \
-    -scheme MMKV \
-    -configuration Release \
-    -destination "generic/platform=tvOS" \
-    -archivePath "archives/MMKV-tvOS" \
-    SKIP_INSTALL=NO \
-    BUILD_LIBRARY_FOR_DISTRIBUTION=YES
+# xcodebuild archive \
+#     -workspace MMKV.xcworkspace \
+#     -scheme MMKV \
+#     -configuration Release \
+#     -destination "generic/platform=tvOS" \
+#     -archivePath "archives/MMKV-tvOS" \
+#     SKIP_INSTALL=NO \
+#     BUILD_LIBRARY_FOR_DISTRIBUTION=YES
 
-xcodebuild archive \
-    -workspace MMKV.xcworkspace \
-    -scheme MMKV \
-    -configuration Release \
-    -destination "generic/platform=macOS" \
-    -archivePath "archives/MMKV-macOS" \
-    SKIP_INSTALL=NO \
-    BUILD_LIBRARY_FOR_DISTRIBUTION=YES
+# xcodebuild archive \
+#     -workspace MMKV.xcworkspace \
+#     -scheme MMKV \
+#     -configuration Release \
+#     -destination "generic/platform=macOS" \
+#     -archivePath "archives/MMKV-macOS" \
+#     SKIP_INSTALL=NO \
+#     BUILD_LIBRARY_FOR_DISTRIBUTION=YES
 
 xcodebuild \
     -create-xcframework \
     -archive archives/MMKV-iOS.xcarchive -framework MMKV.framework \
-    -archive archives/MMKV-iOS-Simulator.xcarchive -framework MMKV.framework \
-    -archive archives/MMKV-tvOS.xcarchive -framework MMKV.framework \
-    -archive archives/MMKV-macOS.xcarchive -framework MMKV.framework \
+    # -archive archives/MMKV-iOS-Simulator.xcarchive -framework MMKV.framework \
+    # -archive archives/MMKV-tvOS.xcarchive -framework MMKV.framework \
+    # -archive archives/MMKV-macOS.xcarchive -framework MMKV.framework \
     -output archives/MMKV.xcframework
 
 zip -r -X "archives/$MMKV_XC_MANIFEST_NAME" "archives/MMKV.xcframework"
